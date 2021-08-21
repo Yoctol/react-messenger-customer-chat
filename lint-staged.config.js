f49729f7401614196d13225d2b4860bf5c774c7f
chentsulin <chentsulin@gmail.com>
2021-08-21 22:43:35 +0800

    prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "*package.json": ["prettier-package-json --write"],
-  "*.(js|ts)": ["eslint --fix", "prettier --write"],
-  "*.(md|json|yml)": ["prettier --write"],
+  '*package.json': ['prettier-package-json --write'],
+  '*.(js|ts)': ['eslint --fix', 'prettier --write'],
+  '*.(md|json|yml)': ['prettier --write'],
 };
