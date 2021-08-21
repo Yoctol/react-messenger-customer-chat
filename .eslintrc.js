fbba529bc8f36c41688d9e55b2cf32a3b6e5cd7c
chentsulin <chentsulin@gmail.com>
2021-08-21 23:18:25 +0800

    babel 7

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['yoctol', 'prettier'],
+  parser: "@babel/eslint-parser",
+  extends: ["yoctol", "prettier"],
   env: {
     browser: true,
     node: true,
     jest: true,
     jasmine: true,
   },
-  plugins: ['prettier'],
+  plugins: ["prettier"],
   rules: {
-    'class-methods-use-this': 'off',
-    'consistent-return': 'off',
-    'global-require': 'off',
-    'no-param-reassign': 'off',
-    'prefer-destructuring': 'off',
-    'prettier/prettier': [
-      'error',
+    "class-methods-use-this": "off",
+    "consistent-return": "off",
+    "global-require": "off",
+    "no-param-reassign": "off",
+    "prefer-destructuring": "off",
+    "prettier/prettier": [
+      "error",
       {
-        trailingComma: 'es5',
+        trailingComma: "es5",
         singleQuote: true,
       },
     ],
-    'react/sort-prop-types': 'off',
+    "react/sort-prop-types": "off",
   },
 };
