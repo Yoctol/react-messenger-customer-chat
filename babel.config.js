27f541bcde3d1be1d31515a4d52add621784d083
chentsulin <chentsulin@gmail.com>
2021-08-21 23:33:10 +0800

    add prettier.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: "defaults" }],
-    "@babel/preset-react",
+    ['@babel/preset-env', { targets: 'defaults' }],
+    '@babel/preset-react',
   ],
 };
